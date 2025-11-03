e21f0913289c18f7a012412b7c9e9e06729b83e9
AndyXue <a931019ing@gmail.com>
2025-11-03 21:30:50 +0800

    add the sensor model of minibot and format the BayesFilter.py

--- scripts/utils/BayesFilter.py
+++ scripts/utils/BayesFilter.py
@@ -4,25 +4,38 @@ import numpy as np
 
 
 class BayesFilter:
-    def __init__(self, initial_belief: np.ndarray = None):
+    def __init__(self, initial_belief: np.ndarray = None, robot: str = 'turtlebot'):
         # Motion model
         # 假設狀態不太會改變
         self.T = np.array(
-            [[0.8, 0.1, 0.1],  # P(x_t|x_{t-1}=0)
-             [0.1, 0.8, 0.1],  # P(x_t|x_{t-1}=1)
-             [0.1, 0.1, 0.8]]  # P(x_t|x_{t-1}=2)
+            [
+                [0.8, 0.1, 0.1],  # P(x_t|x_{t-1}=0)
+                [0.1, 0.8, 0.1],  # P(x_t|x_{t-1}=1)
+                [0.1, 0.1, 0.8],  # P(x_t|x_{t-1}=2)
+            ]  
         )
         # Sensor model
-        self.Z = np.array(
-            [[0.9950, 0.0021, 0.0029], # P(z_t|x_t=0)
-             [0.0454, 0.9546, 0.0],    # P(z_t|x_t=1)
-             [0.0136, 0.0, 0.9864],]   # P(z_t|x_t=2)
-        )
-        
+        if robot == 'turtlebot':
+            self.Z = np.array(
+                [
+                    [0.9950, 0.0021, 0.0029],  # P(z_t|x_t=0)
+                    [0.0454, 0.9546,    0.0],  # P(z_t|x_t=1)
+                    [0.0136,    0.0, 0.9864],  # P(z_t|x_t=2)
+                ]  
+            )
+        else:
+            self.Z = np.array(
+                [
+                    [   1.0,    0.0,    0.0],  # P(z_t|x_t=0)
+                    [0.8593, 0.1407,    0.0],  # P(z_t|x_t=1)
+                    [0.3983,    0.0, 0.6017],  # P(z_t|x_t=2)
+                ]
+            )
+
         if initial_belief is not None:
             self.belief = initial_belief
         else:
-            self.belief = np.array([1/3, 1/3, 1/3])  # 初始信念均等分配
+            self.belief = np.array([1 / 3, 1 / 3, 1 / 3])  # 初始信念均等分配
 
     def update(self, z_t_proba: np.ndarray):
         """
@@ -47,5 +60,5 @@ class BayesFilter:
         else:
             # 如果機率總和趨近於零，重設為均勻分佈以避免錯誤
             self.belief = np.ones_like(self.belief) / len(self.belief)
-            
+
         return self.belief
